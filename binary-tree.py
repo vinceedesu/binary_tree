5a16dbf197ff01773c0b4d1371e3a4030f857e20
vinceedesu <55903157+vinceedesu@users.noreply.github.com>
2023-01-14 18:23:54 +0800

    1

---
+++ binary-tree.py
@@ -0,0 +1,24 @@
+class BinarySearchTree:
+    def __init__(self, data):
+        self.data = data
+        self.right = None
+        self.left = None
+
+    def add_child(self, data):
+        if data == self.data:
+            return
+        
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTree(data)
+            
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTree(data)
+    
+    def in_order_traversal(self):
+        
