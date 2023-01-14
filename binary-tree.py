ef252f0b07c0af14863aa53bac8c8ec9637d55b3
vinceedesu <55903157+vinceedesu@users.noreply.github.com>
2023-01-14 18:32:31 +0800

    Update binary-tree.py

--- binary-tree.py
+++ binary-tree.py
@@ -21,4 +21,27 @@ class BinarySearchTree:
                 self.right = BinarySearchTree(data)
     
     def in_order_traversal(self):
+        elements = []
         
+        if self.left:
+            elements += self.left.in_order_traversal()
+            
+        elements.append(self.data)
+        
+        if self.right:
+            elements += self.right.in_order_traversal()
+        
+        return elements
+
+def build_tree(elements):
+    root = BinarySearchTree(elements[0])
+    
+    for i in range(1, len(elements)):
+        root.add_child(elements[i])
+        
+    return root
+    
+if __name__ == '__main__':
+    numbers = [17, 4 , 1, 20, 9, 23, 18, 34]
+    numbers_tree = build_tree(numbers)
+    print(numbers_tree.in_order_traversal())
